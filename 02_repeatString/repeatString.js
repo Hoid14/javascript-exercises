1fd6e3fc5347809c5496756cf404679586d773db
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-08-11 21:53:20 -0500

    haciendo los ejercicios

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(cadena,numero) {
+    let aux=""
+    if(numero<0){
+        aux="ERROR"
+    }
+    else{
+        for(let i=0;i<numero;i++){
+            aux+=cadena
+        }
+    }
+    return aux
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(cadena) {
+    let a=cadena.split("")
+    a=a.reverse()
+    a=a.join("")
+    return a
 };
 
 // Do not edit below this line
