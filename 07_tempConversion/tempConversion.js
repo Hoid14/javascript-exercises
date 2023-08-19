a0c72cc58781898ca166840ee23950bce3cf24e6
Renarin <122590691+Renarin14@users.noreply.github.com>
2023-08-18 21:55:50 -0500

    termine hasta el ejercicio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let result
+  result=(f-32)/1.8
+  result=result.toFixed(1)
+  result=Number(result)
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let result
+  result=(c*1.8)+32
+  result=result.toFixed(1)
+  result=Number(result)
+  return result
 };
 
 // Do not edit below this line
